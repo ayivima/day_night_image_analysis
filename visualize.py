7a5f45aae2dd8d92811712ff50edff6caf8f89ed
ayivima <48770118+ayivima@users.noreply.github.com>
2019-10-21 12:55:29 +0000

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -2,6 +2,10 @@
 import matplotlib.pyplot as plt
 
 def fsep(features_, sep_coords=([25, 129], [200, 50])):
+    """Plots day and night images as clusters in 2D space for given features.
+    And, draws a line of separation
+    """
+    
     night = df2.loc[df2["labels"]==0, features_]
     day = df2.loc[df2["labels"]==1, features_]
 
@@ -10,7 +14,7 @@ def fsep(features_, sep_coords=([25, 129], [200, 50])):
     groups = "Night", "Day"
 
     fig = plt.figure()
-    ax = fig.add_subplot(1, 1, 1, axisbg="1.0")
+    ax = fig.add_subplot(1, 1, 1)
 
     for data, color, group in zip(data, colors, groups):
         x, y = data[features_[0]], data[features_[1]]
@@ -25,7 +29,7 @@ def fsep(features_, sep_coords=([25, 129], [200, 50])):
         )
         
     x_, y_ = sep_coords
-    plt.plot(x_, y_, "k-")
+    plt.plot(x_, y_)
     plt.title('Night and Day Images Feature Separation')
     plt.xlabel(features_[0])
     plt.ylabel(features_[1])
